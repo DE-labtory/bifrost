f2706003408f54237918d8699aa01f8fee794f70
park hae sung <hea9549@gmail.com>
2018-05-03 16:12:55 +0900

    init client func

--- client.go
+++ client.go
@@ -6,52 +6,67 @@ import (
 	"github.com/it-chain/heimdall/key"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"context"
+	"github.com/it-chain/bifrost/mux"
 )
 
-const defaultTimeout = time.Second * 3
+// defaultDialTimeout
+const (
+	defaultDialTimeout   = 3 * time.Second
+	REQUEST_CONNINFO     = "/requestConnInfo"
+	CONNECTION_ESTABLISH = "/connectionEstablish"
+)
 
+// Client struct. 하나의 연결당 하나의 client 가 필요.
 type Client struct {
+	ServerIp  string
+	ServerKey key.PubKey
+	clientKey key.PriKey
+	conn      Connection
+	mux       mux.Mux
 }
 
+// Client struct 를 생성하는 함수. 연결할 ip, 클라이언트의 private key, message 처리 mux 가 필요함
+func NewClient(serverIp string, clientKey key.PriKey, mux mux.Mux) *Client {
+	return &Client{
+		ServerIp:  serverIp,
+		clientKey: clientKey,
+		mux: mux,
+	}
+}
+
+// Server 와 연결시 사용되는 option.
 type GrpcOpts struct {
-	ip         string
-	tslEnabled bool
+	tlsEnabled bool
 	creds      credentials.TransportCredentials
 }
 
-type KeyOpts struct {
-	priKey key.PriKey
-	pubKey key.PubKey
-}
-
-func Dial(grpcOpts GrpcOpts, key KeyOpts) (*grpc.ClientConn, error) {
+// 서버와 연결 요청. 실패시 err. handshake 과정을 거침.
+func (c Client) ConnectToServer(grpcOpts GrpcOpts) error {
 
 	var opts []grpc.DialOption
 
-	if grpcOpts.tslEnabled {
+	if grpcOpts.tlsEnabled {
 		opts = append(opts, grpc.WithTransportCredentials(grpcOpts.creds))
 	} else {
 		opts = append(opts, grpc.WithInsecure())
 	}
 
-	opts = append(opts, grpc.WithTimeout(defaultTimeout))
-	gconn, err := grpc.Dial(grpcOpts.ip, opts...)
+	dialContext, _ := context.WithTimeout(context.Background(), defaultDialTimeout)
+	gconn, err := grpc.DialContext(dialContext, c.ServerIp, opts...)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	connect(gconn)
-
-	streamWrapper, err := NewClientStreamWrapper(gconn)
+	streamWrapper, err := connect(gconn)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	conn := NewConnection()
-
-	return conn, err
+	
+	return err
 }
 
 func connect(conn *grpc.ClientConn) (StreamWrapper, error) {
