5215d7f2a4321b7b90b564e3036f8c07bf027068
Park Hae Sung <hea9549@gmail.com>
2018-05-05 23:32:40 +0900

    server message type error fixed

--- server.go
+++ server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/it-chain/heimdall/key"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"time"
 )
 
 type Server struct {
@@ -35,7 +36,7 @@ func (s Server) BifrostStream(streamServer pb.StreamService_BifrostStreamServer)
 		return err
 	}
 
-	if m, err := recvWithTimeout(3, streamServer); err == nil {
+	if m, err := RecvWithTimeout(3*time.Second, streamServer); err == nil {
 
 		valid, ip, peerKey := ValidateRequestPeerInfo(m)
 
@@ -72,7 +73,7 @@ func (s Server) BifrostStream(streamServer pb.StreamService_BifrostStreamServer)
 
 func ValidateRequestPeerInfo(envelope *pb.Envelope) (bool, string, key.PubKey) {
 
-	if envelope.GetType() != pb.Envelope_REQUEST_PEERINFO {
+	if envelope.GetType() != pb.Envelope_RESPONSE_PEERINFO {
 		log.Printf("Invaild message type")
 		return false, "", nil
 	}
