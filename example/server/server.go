99dde7282f5579a94eaa8fa94bd051b2097d631a
jun <leebduk@gmail.com>
2018-05-05 13:42:27 +0900

    fixed untracked files

--- example/server/server.go
+++ example/server/server.go
@@ -3,22 +3,18 @@ package main
 import (
 	"log"
 
-	"os"
-
 	"github.com/it-chain/bifrost"
 	"github.com/it-chain/bifrost/mux"
 	"github.com/it-chain/heimdall/key"
 )
 
-var keyPath = "./SERVER_KEY_PATH"
 var ip = "127.0.0.1:7777"
 
 var DefaultMux *mux.DefaultMux
 
 func main() {
 
-	km, err := key.NewKeyManager(keyPath)
-	defer os.RemoveAll(keyPath)
+	km, err := key.NewKeyManager("")
 
 	if err != nil {
 		log.Fatal(err.Error())
