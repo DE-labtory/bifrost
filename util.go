24bba99d8ee63a2bd339e2bf026bca16b3c83e63
Park Hae Sung <hea9549@gmail.com>
2018-05-05 22:11:28 +0900

    receveWithTimeout public으로 변경, 파라미터 time.Duration 을 받도록 변경

--- util.go
+++ util.go
@@ -34,9 +34,9 @@ func ByteToPubKey(byteKey []byte, keyGenOpt key.KeyGenOpts) (key.PubKey, error) 
 	return pubKey, nil
 }
 
-func recvWithTimeout(seconds int, stream Stream) (*pb.Envelope, error) {
+func RecvWithTimeout(timeout time.Duration, stream Stream) (*pb.Envelope, error) {
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(seconds)*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
 	c := make(chan *pb.Envelope, 1)
